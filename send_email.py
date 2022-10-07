12ce725d8376c29fffb3fc38578a844f14c17710
Jonathan Li <30177086+MonliH@users.noreply.github.com>
2022-10-06 23:26:11 -0400

    Initial Commit

---
+++ send_email.py
@@ -0,0 +1,39 @@
+from os import path, getenv
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+gmail_user = getenv("GMAIL_USER")
+gmail_password = getenv("GMAIL_PASSWORD")
+
+server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+server.ehlo()
+server.login(gmail_user, gmail_password)
+
+basepath = path.dirname(__file__)
+static = path.abspath(path.join(basepath, "static"))
+
+with open(path.join(static, "verify.html"), "r") as f:
+    verify_text = f.read()
+
+
+def send(to: str, subject: str, html: str):
+    sent_from = gmail_user
+
+    msg = MIMEMultipart("alternative")
+    msg["From"] = sent_from
+    msg["To"] = to
+    msg["Subject"] = subject
+
+    html_part = MIMEText(html, "html")
+    msg.attach(html_part)
+
+    server.sendmail(gmail_user, to, msg.as_string())
+
+
+def send_verify(to: str, name: str, server: str, code: str):
+    send(
+        to,
+        f"Verify for \"{server}\"",
+        verify_text.replace("{{name}}", name).replace("{{server}}", server).replace("{{code}}", code),
+    )
